ed0a1357178e96aa6e7ea51f1c65571f7fb952b7
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-01-22 16:15:53 -0800

    delete removes updates

--- methods/project/delete.py
+++ methods/project/delete.py
@@ -5,5 +5,6 @@ from toto.events import EventManager
 @requires('name')
 def invoke(handler, params):
   handler.connection.db.projects.remove({'name': params['name']})
+  handler.connection.db.updates.remove({'name': params['name']})
   EventManager.instance().send("project_updated", {'name': params['name']})
   return {'name': params['name']}
